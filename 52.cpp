01abbd6e5aea9577c64b069941ab3d1eac796d66
Pravir Kadian <pravirkadian123@gmail.com>
2021-12-04 17:53:21 +0530

    Add files via upload

---
+++ 52.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool is_valid(vector<string> &board, int row, int col){
+        // check col
+        for(int i=row;i>=0;--i)
+            if(board[i][col] == 'Q') return false;
+        // check left diagonal
+        for(int i=row,j=col;i>=0&&j>=0;--i,--j)
+            if(board[i][j] == 'Q') return false;
+        //check right diagonal
+        for(int i=row,j=col;i>=0&&j<board.size();--i,++j)
+            if(board[i][j] == 'Q') return false;
+        return true;
+    }
+    void solve(int &ans,vector<string>&board,int row,int n){
+        if(row==n){
+            ans++;
+            return;
+        }
+        for(int col=0;col<n;col++){
+            if(is_valid(board,row,col)){
+                board[row][col]='Q';
+                solve(ans,board,row+1,n);
+                board[row][col]='.';
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        int ans = 0;
+        vector<string>board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            board[i]=s;
+        }
+        solve(ans,board,0,n);
+        return ans;
+    }
+};
